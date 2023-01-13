3accfea50a68e08bf49895d62ae8bace4f20b519
Shreya Suresh <shreya4763@gmail.com>
2023-01-13 14:18:53 -0800

    testing that code works, which it does

--- main.cpp
+++ main.cpp
@@ -5,6 +5,28 @@
 
 using namespace std;
 
+void add(char* name);
+void print(Node* next);
+
+Node* head = NULL;
+
 int main() {
+  char* name = new char[20];
+  strcpy(name, "Tyler");
+  Student* stu1 = new Student(name);
+  strcpy(name, "Jenna");
+  Student* stu2 = new Student(name);
+
+  
+  Node* head =  new Node(stu1);
+  head->setStudent(stu1);
+  cout << "On first: ";
+  cout << head->getStudent() << endl;
+
+  Node* current = head;
+  current->setNext(new Node(stu2));
+  current->getNext()->setStudent(stu2);
+  cout << "On second: ";
+  cout << head->getNext()->getStudent() << endl;
 
 }
